82e7c608eb62fffa9212624421f6a29d5d0e6659
warren <5959690+wrn14897@users.noreply.github.com>
2022-09-20 13:25:25 -0700

    fix: update swap directory

--- .config/nvim/lua/settings.lua
+++ .config/nvim/lua/settings.lua
@@ -20,7 +20,7 @@ vim.o.t_SI = "<Esc>[6 q"
 vim.o.t_SR = "<Esc>[4 q"
 vim.o.t_EI = "<Esc>[2 q"
 
-set.directory = "$HOME/.local/state/nvim/swap//" --- default
+set.directory = "~/.local/state/nvim/swap//" --- default
 set.encoding = "utf-8"
 set.mousehide = true
 set.shortmess :append('filmnrxoOtT') --- Abbrev. of messages (avoids 'hit enter')
